4f9dbe363cb06fe17006a3c52d11b7dac437c95d
Jose Sarmiento <jose.dev.official@gmail.com>
2022-04-20 13:21:19 +0800

    Remove index method conflict in dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-=======
+
 use DB;
->>>>>>> origin/master
+
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     /**
-<<<<<<< HEAD
      * Create a new controller instance.
      *
      * @return void
@@ -29,24 +27,5 @@ class DashboardController extends Controller
     public function index()
     {
         return view('dashboard');
-=======
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        $total_revenue = DB::table('orders')->sum('total_payment');
-        $total_orders = DB::table('orders')->count();
-        $total_customers = DB::table('customers')->count();
-            
-        return response()->json([
-            "total" => [
-                "revenue" => $total_revenue,
-                "orders" => $total_orders,
-                "customers" => $total_customers,
-            ]
-        ]);
->>>>>>> origin/master
     }
 }
